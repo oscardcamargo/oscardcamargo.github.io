8efef8633bf7b61a5912b2ec447d27e572ad2fe9
oscarcamargo <oscardcamargo@gmail.com>
2025-05-30 12:20:45 -0400

    Added organoid interface

--- js/sketch.js
+++ js/sketch.js
@@ -9,7 +9,9 @@ var voronoi = new Voronoi();
 
 let guiParams = {
     "backgroundColor" : [255, 255, 255],
+    "number of cells": 12,
     "cell size" : 60,
+    "organoid size" : 100,
     "show voronoi" : false,
     "show cell colors" : true,
     "show cell centers" : true,
@@ -30,7 +32,9 @@ let tempShape;
 function createGui(){
     gui = new lil.GUI();
     gui.addColor(guiParams, 'backgroundColor', 255)
+    gui.add(guiParams, 'number of cells', 1, 100, 1)
     gui.add(guiParams, 'cell size', 1, 200, 1)
+    gui.add(guiParams, 'organoid size', 1, 250, 1)
     gui.add(guiParams, 'show voronoi')
     gui.add(guiParams, 'show cell colors')
     gui.add(guiParams, 'show cell centers')
@@ -44,7 +48,30 @@ function reset(p5){
     for (let i = 0; i < physics.behaviors.length; i++){
         physics.removeBehavior(physics.behaviors[0]);
     }
-    cells = []
+    // Remove existing GUI
+    if (gui) {
+        gui.destroy();
+    }
+
+    // Reset parameters
+    guiParams = {
+        "backgroundColor": [255, 255, 255],
+        "number of cells": 12,
+        "cell size": 60,
+        "organoid size": 100,
+        "show voronoi": false,
+        "show cell colors": true,
+        "show cell centers": true,
+        "show cell boundary": true,
+    };
+
+    // Add back the Reset function
+    guiParams['Reset'] = function(){
+        reset(p5);
+    };
+
+    // Recreate GUI
+    createGui();
 }
 
 function createCell(x, y, size){
@@ -73,19 +100,19 @@ function drawCells(p5, cells){
 function sortCells(cells){
     cells.sort((a, b) => {
         if (a.x === b.x) {
-          return a.y - b.y; // Compare `y` if `x` is the same
+            return a.y - b.y; // Compare `y` if `x` is the same
         }
         return a.x - b.x; // Compare `x`
-      });
+    });
 }
 
 function sortVoronoiCells(cells){
     cells.sort((a, b) => {
         if (a.site.x === b.site.x) {
-          return a.site.y - b.site.y; // Compare `y` if `x` is the same
+            return a.site.y - b.site.y; // Compare `y` if `x` is the same
         }
         return a.site.x - b.site.x; // Compare `x`
-      });
+    });
 }
 
 function clipCells(cells, voronoiCells){
@@ -96,7 +123,7 @@ function clipCells(cells, voronoiCells){
         let clipper = new ConvexPolygonClipper(clipCell);
         let clippedCell;
         if (clipCell.vertices.length > 0){
-            clippedCell = clipper.clipPolygon(poly);  
+            clippedCell = clipper.clipPolygon(poly);
         } else {
             clippedCell = poly;
         }
@@ -142,12 +169,12 @@ function voronoiCellToPolygon(cell){
 
 
 new p5(function(p5){
-p5.setup = function(){
-    guiParams['Reset'] = function(){
-        reset(p5);
-    }
-    let canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
-    physics = new toxi.physics2d.VerletPhysics2D();
+    p5.setup = function(){
+        guiParams['Reset'] = function(){
+            reset(p5);
+        }
+        let canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight);
+        physics = new toxi.physics2d.VerletPhysics2D();
         // physics.addBehavior(new GravityBehavior(new toxi.geom.Vec2D(0, 0.1)));
         // add drag
         physics.setDrag(0.5);
@@ -159,43 +186,57 @@ p5.setup = function(){
         tempShape = new Circle(new Vec2D(p5.width/2, p5.height/2), 100).toPolygon2D(6);
 
 
-    p5.strokeWeight(2);
-
-}
-
-
-
-p5.draw = function(){
-
-    physics.update();
-    p5.background(guiParams['backgroundColor']);
-    p5.noFill();
-    // var sites = cells.map((cell) => ({x: cell.x, y: cell.y}));
-    var voronoiDiagram = voronoi.compute(cells, {xl: 0, xr: p5.width, yt: 0, yb: p5.height})
-    sortCells(cells);
-    sortVoronoiCells(voronoiDiagram.cells);
-    voronoiCells = voronoiDiagram.cells.map(voronoiCellToPolygon);
-    
-    if(guiParams['show voronoi']){
-        for(let i = 0; i < voronoiCells.length; i++){
-            // p5.fill(colors[i % colors.length]);
-            p5.noFill();
-            drawPoly(p5, voronoiCells[i]);
-        }
+        p5.strokeWeight(2);
     }
-    // drawVoronoi(p5, voronoiDiagram);
-    
-    drawCells(p5, cells);
-    // drawPoly(p5, tempShape);
-    
-}
 
-p5.windowResized = function() {
-    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
-};
 
-p5.doubleClicked = function(){
-    createCell(p5.mouseX, p5.mouseY, guiParams["cell size"]);
-}
+
+    p5.draw = function(){
+        physics.update();
+        p5.background(guiParams['backgroundColor']);
+        p5.noFill();
+        p5.drawOrganoid();
+        // var sites = cells.map((cell) => ({x: cell.x, y: cell.y}));
+        var voronoiDiagram = voronoi.compute(cells, {xl: 0, xr: p5.width, yt: 0, yb: p5.height})
+        sortCells(cells);
+        sortVoronoiCells(voronoiDiagram.cells);
+        voronoiCells = voronoiDiagram.cells.map(voronoiCellToPolygon);
+
+        if(guiParams['show voronoi']){
+            for(let i = 0; i < voronoiCells.length; i++){
+                // p5.fill(colors[i % colors.length]);
+                p5.noFill();
+                drawPoly(p5, voronoiCells[i]);
+            }
+        }
+        // drawVoronoi(p5, voronoiDiagram);
+
+        drawCells(p5, cells);
+        // drawPoly(p5, tempShape);
+
+    }
+
+    p5.windowResized = function() {
+        p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    };
+
+// p5.doubleClicked = function(){
+//     createCell(p5.mouseX, p5.mouseY, guiParams["cell size"]);
+// };
+
+    p5.drawOrganoid = function(){
+        cells = [];
+        let centerX = p5.width / 2;
+        let centerY = p5.height / 2;
+        let radius = guiParams["organoid size"]; // Radius of the organoid
+        let N = guiParams["number of cells"];       // Number of cells
+
+        for (let i = 0; i < N; i++) {
+            let angle = p5.TWO_PI * (i / N);
+            let x = centerX + radius * Math.cos(angle);
+            let y = centerY + radius * Math.sin(angle);
+            createCell(x, y, guiParams["cell size"]);
+        }
+    };
 
 });
